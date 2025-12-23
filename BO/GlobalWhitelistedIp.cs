326399f78bd4e82714d05d12dc0b5221e0e37a0b
Kacper Marcinkiewicz <marcinkiewicz.kacper@gmail.com>
2025-12-23 23:15:30 +0100

    IP whitelist basic UI and BO

---
+++ BO/GlobalWhitelistedIp.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PangolinWatchdog.BO;
+
+public class GlobalWhitelistedIp
+{
+    [Key]
+    public long Id { get; set; }
+    
+    [Required]
+    [MaxLength(45)]
+    public string IpAddress { get; set; } = string.Empty;
+    
+    public string? Name { get; set; } = string.Empty;
+}
---
+++ Migrations/20251223204807_GlobalIpWhitelist.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PangolinWatchdog.Migrations
+{
+    /// <inheritdoc />
+    public partial class GlobalIpWhitelist : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GlobalWhitelistedIps",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IpAddress = table.Column<string>(type: "TEXT", maxLength: 45, nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GlobalWhitelistedIps", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GlobalWhitelistedIps");
+        }
+    }
+}
