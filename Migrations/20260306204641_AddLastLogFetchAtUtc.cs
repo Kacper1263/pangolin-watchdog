0fea2338de1cd2591ca865ecefca3be469cf24d0
Kacper Marcinkiewicz <marcinkiewicz.kacper@gmail.com>
2026-03-06 21:51:53 +0100

    Persist last log fetch time

---
+++ Migrations/20260306204641_AddLastLogFetchAtUtc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PangolinWatchdog.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastLogFetchAtUtc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLogFetchAtUtc",
+                table: "Configurations",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastLogFetchAtUtc",
+                table: "Configurations");
+        }
+    }
+}
